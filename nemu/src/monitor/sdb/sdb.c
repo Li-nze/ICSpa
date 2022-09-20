5f9cf5d1be5b8e09cc3d6f42fe572665e5c06abc
tracer-ics2022 <tracer@njuics.org>
2022-09-20 22:51:03 +0800

    >  compile NEMU 211300034 何临哲 Linux lin-ThinkPad-Edge-E540 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:51:03 up  1:24,  4 users,  load average: 0.29, 0.20, 0.19

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -49,8 +49,9 @@ static int cmd_c(char *args) {
 
 
 static int cmd_q(char *args) {
-  cpu_exec(-1);
-  return -1;
+  //cpu_exec(-1);
+  //return -1;
+  return 0;
 }
 
 static int cmd_help(char *args);
