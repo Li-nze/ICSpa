9bd45c2e84edeef0ab8733175f3b8033064117f2
tracer-ics2022 <tracer@njuics.org>
2022-09-21 09:04:07 +0800

    >  compile NEMU 211300034 何临哲 Linux lin-ThinkPad-Edge-E540 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:04:07 up 54 min,  5 users,  load average: 0.13, 0.24, 0.19

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -43,7 +43,9 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 static void exec_once(Decode *s, vaddr_t pc) {
   s->pc = pc;
   s->snpc = pc;
+  printf("0: %d\n",nemu_state.state);
   isa_exec_once(s);
+  printf("1: %d\n",nemu_state.state);
   cpu.pc = s->dnpc;
 #ifdef CONFIG_ITRACE
   char *p = s->logbuf;
