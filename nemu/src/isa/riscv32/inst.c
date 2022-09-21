63490fe112d0c3b9a577641791ae5bede68b921c
tracer-ics2022 <tracer@njuics.org>
2022-09-21 09:28:32 +0800

    >  compile NEMU 211300034 何临哲 Linux lin-ThinkPad-Edge-E540 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:28:32 up  1:18,  5 users,  load average: 0.43, 0.29, 0.22

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -47,13 +47,9 @@ static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, wor
 }
 
 static int decode_exec(Decode *s) {
-  printf("%d\n",nemu_state.state);
   int dest = 0;
-  printf("%d\n",nemu_state.state);
   word_t src1 = 0, src2 = 0, imm = 0;
-  printf("%d\n",nemu_state.state);
   s->dnpc = s->snpc;
-  printf("%d\n",nemu_state.state);
 
 #define INSTPAT_INST(s) ((s)->isa.inst.val)
 #define INSTPAT_MATCH(s, name, type, ... /* execute body */ ) { \
