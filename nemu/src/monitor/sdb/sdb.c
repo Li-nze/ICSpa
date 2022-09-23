8ed682151a5826589fe080d47f6cd02a3f589106
tracer-ics2022 <tracer@njuics.org>
2022-09-23 18:01:04 +0800

    >  compile NEMU 211300034 何临哲 Linux lin-ThinkPad-Edge-E540 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:01:04 up  1:42,  6 users,  load average: 1.36, 0.95, 0.49

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -70,14 +70,15 @@ static int cmd_x(char *args){
 		return 0;
 	}
 	else{
+		unsigned int my_indent=10;
 		paddr_t add;
 		sscanf(arg, "%x", &add);
 		printf("%x\n",add);
 		word_t res;
 		for(int i=0;i<n;++i){
 			res=paddr_read(add,4);
+			printf("%#-*x%#-*x\n",my_indent, add, my_indent, add);
 			add+=4;
-			printf("%x\n",add);
 			printf("%x\n",res);
 		}
 		return 0;
