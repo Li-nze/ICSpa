6e7afbd35c8de2f9936e0318230e50697da65c33
tracer-ics2022 <tracer@njuics.org>
2022-09-30 18:39:24 +0800

    >  compile NEMU 211300034 何临哲 Linux lin-ThinkPad-Edge-E540 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:39:24 up  2:37,  6 users,  load average: 0.58, 0.37, 0.29

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -263,7 +263,7 @@ void init_sdb() {
 	  word_t b=expr(s, success);
 	  if(a!=b){
 		  FILE *fp1=NULL;
-		  fp1=fopen("~/pa1error.txt", "a");
+		  fp1=fopen("/tmp/pa1error.txt", "a");
 		  assert(fp1!=NULL);
 		  fprintf(fp1,"k:%d expr:%u gen:%u %s\n", k, b, a, s );
 		  i=fclose(fp1);
