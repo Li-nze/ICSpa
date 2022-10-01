739ff2a8a270397f6856fb4c80470cc3c3e7df83
tracer-ics2022 <tracer@njuics.org>
2022-10-01 18:02:35 +0800

    >  compile NEMU 211300034 何临哲 Linux lin-ThinkPad-Edge-E540 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:02:35 up  9:29,  9 users,  load average: 0.32, 0.27, 0.23

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -46,8 +46,8 @@ void new_wp(char *ex, word_t va){
 		else{head=a;}
 		tail=a;
 		//printf("ex:%s %ld %ld\n", ex, strlen(ex), sizeof(ex));
-		//a->wpexpr=(char *)malloc(sizeof(ex)+sizeof(ex[0]));
-		a->wpexpr=(char *)malloc((strlen(ex)+1)*sizeof(ex[0]));
+		a->wpexpr=(char *)malloc(sizeof(ex)+sizeof(ex[0]));
+		//a->wpexpr=(char *)malloc((strlen(ex)+1)*sizeof(ex[0]));
 		//printf("aaa\n");
 		//printf("ex:%s %ld %ld\n", ex, strlen(ex), sizeof(ex));
 		strcpy(a->wpexpr, ex);
