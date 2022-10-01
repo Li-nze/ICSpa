dd76b19ba6b613c51cd075fd269fa07fb326a6eb
tracer-ics2022 <tracer@njuics.org>
2022-10-01 17:05:54 +0800

    >  compile NEMU 211300034 何临哲 Linux lin-ThinkPad-Edge-E540 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:05:54 up  8:33,  9 users,  load average: 0.67, 0.41, 0.28

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -44,7 +44,7 @@ void new_wp(char *ex, word_t va){
 		if(tail!=NULL){tail->next=a;}
 		else{head=a;}
 		tail=a;
-		a->wpexpr=ex;
+		strcpy(a->wpexpr, ex);
 		a->val=va;
 		printf("Add NO.%d watchpoint successfully.\n", a->NO);
 		bool b=1;
